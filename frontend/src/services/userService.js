b106be275f45e6621b41753a338e4dea05165c20
Arvind-K1 <arvindkandpal2002@gmail.com>
2024-10-13 20:03:09 +0530

    some error resolve

--- frontend/src/services/userService.js
+++
@@ -1,9 +0,0 @@
-import axios from '../services/axiosConfig';
-
-export const submitUser = async (formData) => {
-  return await axios.post('http://localhost:5000/api/users/upload', formData);
-};
-
-export const fetchUsers = async () => {
-  return await axios.get('http://localhost:5000/api/users');
-};
